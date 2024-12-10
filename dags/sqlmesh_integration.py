ff97fa4da67774d40c32dd372f781bcb1087e24c
dorianappex <dorian.banutoiu@joinappex.com>
2024-12-10 18:10:33 +0200

    Update sqlmesh_integration.py

--- dags/sqlmesh_integration.py
+++ dags/sqlmesh_integration.py
@@ -1,10 +1,10 @@
 from sqlmesh.schedulers.airflow.integration import SQLMeshAirflow
 
 sqlmesh_airflow = SQLMeshAirflow("bigquery", default_catalog="appex-data",
-    engine_operator_args={
-        "bigquery_conn_id": "sqlmesh_google_cloud_bigquery_default",
-        "location": "US"
-    },
+    # engine_operator_args={
+    #     "bigquery_conn_id": "sqlmesh_google_cloud_bigquery_default",
+    #     "location": "US"
+    # },
 )
 
 for dag in sqlmesh_airflow.dags:
