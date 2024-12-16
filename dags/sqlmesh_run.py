71cd78acea45d786ace16a2bc63f3794fd05a784
dorianappex <dorian.banutoiu@joinappex.com>
2024-12-16 13:22:58 +0200

    Update sqlmesh_run.py

--- dags/sqlmesh_run.py
+++ dags/sqlmesh_run.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-# Define default arguments for the DAG
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -11,11 +11,11 @@ default_args = {
     'retries': 1,
 }
 
-# Define the DAG
+# Definition
 with DAG(
-    'sqlmesh_bash_dag',  # DAG name
+    'sqlmesh_run',  # DAG name
     default_args=default_args,
-    description='A simple DAG to run sqlmesh',
+    description='DAG to run sqlmesh',
     schedule_interval=None,  # Set to None for manual trigger
     start_date=datetime(2024, 1, 1),  # Replace with your desired start date
     catchup=False,  # Don't backfill
